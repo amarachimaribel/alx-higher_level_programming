de9552c9def135c194825049d7dabc72534f2343
amarachimaribel <amarachimaribel266@gmail.com>
2023-09-05 15:51:45 -0700

    ami commit

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,4 +4,4 @@ def uppercase(str):
     for c in str:
         if ord(c) >= 97 and ord(c) <= 122:
             c = chr(ord(c) - 32)
-        print("{}".format(c), end="")
+        print("{:2}".format(c), end="")
